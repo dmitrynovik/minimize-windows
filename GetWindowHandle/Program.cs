9d93b8227c77167d351db3ab664d87a5f8dcece7
Dmitry Novik <dmitry.novik@gmail.com>
2019-11-06 16:02:35 +1100

    init

---
+++ GetWindowHandle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GetWindowHandle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var winMgr = new WindowManager();
+            var handles = winMgr.GetAllWindowHandles("chrome");
+            foreach (var h in handles)
+            {
+                var result = winMgr.MinimizeWindow(h);
+                Console.WriteLine("Minimizing {0} returned {1}", h, result);
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GetWindowHandle/WindowManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace GetWindowHandle
+{
+    public class WindowManager
+    {
+        delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern bool EnumThreadWindows(int dwThreadId, EnumThreadDelegate lpfn, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        static IEnumerable<IntPtr> EnumWindows(int processId)
+        {
+            var handles = new List<IntPtr>();
+
+            foreach (ProcessThread thread in Process.GetProcessById(processId).Threads)
+                EnumThreadWindows(thread.Id, 
+                    (hWnd, lParam) => { handles.Add(hWnd); return true; }, 
+                    IntPtr.Zero);
+
+            return handles;
+        }
+
+        public IEnumerable<IntPtr> GetAllWindowHandles(string procName) =>
+            Process.GetProcessesByName(procName)
+                .SelectMany(p => EnumWindows(p.Id));
+
+        public bool MinimizeWindow(IntPtr h) => ShowWindow(h, 6);
+    }
+}
